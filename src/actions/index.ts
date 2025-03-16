ea3e7f6b49347911289b6d171efcaf09ead1af2e
Sqot0 <pomi10orka@gmail.com>
2025-03-16 08:51:10 +0100

    env variables using fix

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,8 @@
 import { defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
-import { getSecret } from 'astro:env/server';
 
-const TELEGRAM_BOT_TOKEN = getSecret('TELEGRAM_BOT_TOKEN');
-const TELEGRAM_CHAT_ID = getSecret('TELEGRAM_CHAT_ID');
+const TELEGRAM_BOT_TOKEN =  import.meta.env.TELEGRAM_BOT_TOKEN
+const TELEGRAM_CHAT_ID = import.meta.env.TELEGRAM_CHAT_ID
 
 export const server = {
   sendForm: defineAction({
@@ -13,7 +12,7 @@ export const server = {
       contact: z.string(),
       message: z.string(),
     }),
-    handler: async ({name, contact, message}) => {
+    handler: async ({name, contact, message}, ctx) => {
         const content = `<b>Name:</b> ${name}%0A<b>Contact:</b> ${contact}%0A<b>Message:</b> ${message}`
 
         const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&parse_mode=HTML&text=${content}`)
