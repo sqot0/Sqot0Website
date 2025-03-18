d2cd48cb47ee68de9d95cb9f4dda005cf393b3b5
Sqot0 <pomi10orka@gmail.com>
2025-03-18 18:39:12 +0100

    seo optimisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: "https://sqot0.my.id",
+
   vite: {
     plugins: [tailwindcss()]
   },
-  
+
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  
+  integrations: [sitemap()]
 });
